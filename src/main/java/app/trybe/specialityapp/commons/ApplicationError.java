069e3cf2da1b01f9f7931e25a210c3704603fe73
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-03-17 17:13:14 -0300

    Implementação: classe ApplicationError que retorna o status e mensagem de erro

--- src/main/java/app/trybe/specialityapp/commons/ApplicationError.java
+++ src/main/java/app/trybe/specialityapp/commons/ApplicationError.java
@@ -1,6 +1,22 @@
 package app.trybe.specialityapp.commons;
 
+import javax.ws.rs.core.Response.Status;
+
 /**Classe ApplicationError.*/
 public class ApplicationError {
-
+  private Status status;
+  private String message;
+  
+  public ApplicationError(Status status, String message) {
+    this.status = status;
+    this.message = message;
+  }
+  
+  public Status getStatus() {
+    return status;
+  }
+  
+  public String getMessage() {
+    return message;
+  }
 }
