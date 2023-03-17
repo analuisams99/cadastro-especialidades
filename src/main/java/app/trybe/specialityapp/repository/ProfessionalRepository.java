97f0e2e9f96f1449114e5f730dc6e3e38f7c58f3
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-03-17 17:17:06 -0300

    Implementação: criação da interface ProfessionalRepository estendendo JpaRepository

--- src/main/java/app/trybe/specialityapp/repository/ProfessionalRepository.java
+++ src/main/java/app/trybe/specialityapp/repository/ProfessionalRepository.java
@@ -1,6 +1,9 @@
 package app.trybe.specialityapp.repository;
 
-/**Interface ProfessionalRepository.*/
-public interface ProfessionalRepository {
+import app.trybe.specialityapp.model.Professional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-}
+/**Interface ProfessionalRepository.*/
+@Repository
+public interface ProfessionalRepository extends JpaRepository<Professional, Integer> {}
